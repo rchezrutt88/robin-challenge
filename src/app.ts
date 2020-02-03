667cfed24671afab9c68fb9e6f2bfa794a7ed87e
Robert Ruttenberg <rob@strivelabs.com>
2020-02-03 16:58:25 -0500

    more scaffolding/templating

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express, {
+  NextFunction,
+  Request,
+  Response,
+} from 'express'
+import logger from 'morgan'
+import createHttpError, { HttpError } from 'http-errors'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import indexRouter from './routes'
+import usersRouter from './routes/users'
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../src/views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
@@ -24,11 +27,11 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createHttpError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err: HttpError, req: Request, res: Response, next: NextFunction){
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,4 +41,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+app.listen(3000, () => {
+  console.log('Server listening on port 3000')
+})
