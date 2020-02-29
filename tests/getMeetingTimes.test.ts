f3e05b25d738d6d53c743282e178be7a3dc3799d
Robert Ruttenberg <rob@strivelabs.com>
2020-02-29 17:19:23 -0500

    fix tests

--- tests/getMeetingTimes.test.ts
+++ tests/getMeetingTimes.test.ts
@@ -16,26 +16,28 @@ const appTest = supertest(http.createServer(app));
 
 describe("Meeting Time App", () => {
   describe("challenge two", () => {
-    const interval = Interval.fromISO(
-      "2019-01-01T00:00:00+0000/2019-01-01T23:59:59+0000"
-    );
-    const users = findUsers(1, 2, 3);
-    const meetingIntervals = getMeetingTimes(interval, users);
-    const meetingResponse = transformMeetingTimes(meetingIntervals, {workingHours: true});
-    expect(meetingResponse).toEqual([
-      {
-        end_time: "2019-01-01T11:00:00.000Z",
-        start_time: "2019-01-01T10:17:00.000Z"
-      },
-      {
-        end_time: "2019-01-01T14:00:00.000Z",
-        start_time: "2019-01-01T13:45:00.000Z"
-      },
-      {
-        end_time: "2019-01-01T17:00:00.000Z",
-        start_time: "2019-01-01T16:30:00.000Z"
-      }
-    ]);
+    it("returns the properly adjusted meeting times", () => {
+      const interval = Interval.fromISO(
+        "2019-01-01T00:00:00+0000/2019-01-01T23:59:59+0000"
+      );
+      const users = findUsers(1, 2, 3);
+      const meetingIntervals = getMeetingTimes(interval, users, {workingHours: true});
+      const meetingResponse = transformMeetingTimes(meetingIntervals);
+      expect(meetingResponse).toEqual([
+        {
+          end_time: "2019-01-01T11:00:00.000Z",
+          start_time: "2019-01-01T10:17:00.000Z"
+        },
+        {
+          end_time: "2019-01-01T14:00:00.000Z",
+          start_time: "2019-01-01T13:45:00.000Z"
+        },
+        {
+          end_time: "2019-01-01T17:00:00.000Z",
+          start_time: "2019-01-01T16:30:00.000Z"
+        }
+      ]);
+    });
   });
   describe("challenge one", () => {
     test("it reads user data", () => {
@@ -147,73 +149,73 @@ describe("Meeting Time App", () => {
       );
     });
 
-  test("it finds users by id", () => {
-    const userIds = [1];
-    const users = findUsers(...userIds);
-    expect(users).toEqual([
-      {
-        user_id: 1,
-        time_zone: "UTC",
-        working_hours: {
-          start: "09:00",
-          end: "17:00"
-        },
-        events: [
-          {
-            id: 2,
-            title: "Meeting B",
-            start: "2019-01-01T09:00:00+0000",
-            end: "2019-01-01T10:00:00+0000"
+    test("it finds users by id", () => {
+      const userIds = [1];
+      const users = findUsers(...userIds);
+      expect(users).toEqual([
+        {
+          user_id: 1,
+          time_zone: "UTC",
+          working_hours: {
+            start: "09:00",
+            end: "17:00"
           },
-          {
-            id: 4,
-            title: "Meeting D",
-            start: "2019-01-01T11:00:00+0000",
-            end: "2019-01-01T12:00:00+0000"
-          },
-          {
-            id: 6,
-            title: "Meeting F",
-            start: "2019-01-01T12:00:00+0000",
-            end: "2019-01-01T12:45:00+0000"
-          },
-          {
-            id: 8,
-            title: "Meeting H",
-            start: "2019-01-01T14:00:00+0000",
-            end: "2019-01-01T15:30:00+0000"
-          }
-        ]
-      }
-    ]);
-  });
+          events: [
+            {
+              id: 2,
+              title: "Meeting B",
+              start: "2019-01-01T09:00:00+0000",
+              end: "2019-01-01T10:00:00+0000"
+            },
+            {
+              id: 4,
+              title: "Meeting D",
+              start: "2019-01-01T11:00:00+0000",
+              end: "2019-01-01T12:00:00+0000"
+            },
+            {
+              id: 6,
+              title: "Meeting F",
+              start: "2019-01-01T12:00:00+0000",
+              end: "2019-01-01T12:45:00+0000"
+            },
+            {
+              id: 8,
+              title: "Meeting H",
+              start: "2019-01-01T14:00:00+0000",
+              end: "2019-01-01T15:30:00+0000"
+            }
+          ]
+        }
+      ]);
+    });
 
-  test("getMeetingTimes accepts an array of ids with with a start and end time", () => {
-    const interval = Interval.fromISO(
-      "2019-01-01T00:00:00+0000/2019-01-01T23:59:59+0000"
-    );
-    const users = findUsers(1, 2, 3);
-    const meetingIntervals = getMeetingTimes(interval, users);
-    const meetingResponse = transformMeetingTimes(meetingIntervals);
-    expect(meetingResponse).toEqual([
-      {
-        end_time: "2019-01-01T09:00:00.000Z",
-        start_time: "2019-01-01T00:00:00.000Z"
-      },
-      {
-        end_time: "2019-01-01T11:00:00.000Z",
-        start_time: "2019-01-01T10:17:00.000Z"
-      },
-      {
-        end_time: "2019-01-01T14:00:00.000Z",
-        start_time: "2019-01-01T13:45:00.000Z"
-      },
-      {
-        end_time: "2019-01-01T23:59:59.000Z",
-        start_time: "2019-01-01T16:30:00.000Z"
-      }
-    ]);
-  });
+    test("getMeetingTimes accepts an array of ids with with a start and end time", () => {
+      const interval = Interval.fromISO(
+        "2019-01-01T00:00:00+0000/2019-01-01T23:59:59+0000"
+      );
+      const users = findUsers(1, 2, 3);
+      const meetingIntervals = getMeetingTimes(interval, users);
+      const meetingResponse = transformMeetingTimes(meetingIntervals);
+      expect(meetingResponse).toEqual([
+        {
+          end_time: "2019-01-01T09:00:00.000Z",
+          start_time: "2019-01-01T00:00:00.000Z"
+        },
+        {
+          end_time: "2019-01-01T11:00:00.000Z",
+          start_time: "2019-01-01T10:17:00.000Z"
+        },
+        {
+          end_time: "2019-01-01T14:00:00.000Z",
+          start_time: "2019-01-01T13:45:00.000Z"
+        },
+        {
+          end_time: "2019-01-01T23:59:59.000Z",
+          start_time: "2019-01-01T16:30:00.000Z"
+        }
+      ]);
+    });
 
     test("empty request", async () => {
       await appTest.get("/available_times").expect(400);
@@ -257,111 +259,109 @@ describe("Meeting Time App", () => {
               start_time: "2019-01-01T16:30:00.000Z"
             }
           ]
-<<<<<<< Updated upstream:tests/getMeetingTimes.test.ts
+        });
+    });
+
+    test("it finds users by id", () => {
+      const userIds = [1];
+      const users = findUsers(...userIds);
+      expect(users).toEqual([
+        {
+          user_id: 1,
+          time_zone: "UTC",
+          working_hours: {
+            start: "09:00",
+            end: "17:00"
+          },
+          events: [
+            {
+              id: 2,
+              title: "Meeting B",
+              start: "2019-01-01T09:00:00+0000",
+              end: "2019-01-01T10:00:00+0000"
+            },
+            {
+              id: 4,
+              title: "Meeting D",
+              start: "2019-01-01T11:00:00+0000",
+              end: "2019-01-01T12:00:00+0000"
+            },
+            {
+              id: 6,
+              title: "Meeting F",
+              start: "2019-01-01T12:00:00+0000",
+              end: "2019-01-01T12:45:00+0000"
+            },
+            {
+              id: 8,
+              title: "Meeting H",
+              start: "2019-01-01T14:00:00+0000",
+              end: "2019-01-01T15:30:00+0000"
+            }
+          ]
         }
-      ])
-    );
-  });
+      ]);
+    });
 
-  test("it finds users by id", () => {
-    const userIds = [1];
-    const users = findUsers(...userIds);
-    expect(users).toEqual([
-      {
-        user_id: 1,
-        time_zone: "UTC",
-        working_hours: {
-          start: "09:00",
-          end: "17:00"
+    test("getMeetingTimes accepts an array of ids with with a start and end time", () => {
+      const interval = Interval.fromISO(
+        "2019-01-01T00:00:00+0000/2019-01-01T23:59:59+0000"
+      );
+      const users = findUsers(1, 2, 3);
+      const meetingIntervals = getMeetingTimes(interval, users);
+      const meetingResponse = transformMeetingTimes(meetingIntervals);
+      expect(meetingResponse).toEqual([
+        {
+          end_time: "2019-01-01T09:00:00.000Z",
+          start_time: "2019-01-01T00:00:00.000Z"
         },
-        events: [
-          {
-            id: 2,
-            title: "Meeting B",
-            start: "2019-01-01T09:00:00+0000",
-            end: "2019-01-01T10:00:00+0000"
-          },
-          {
-            id: 4,
-            title: "Meeting D",
-            start: "2019-01-01T11:00:00+0000",
-            end: "2019-01-01T12:00:00+0000"
-          },
-          {
-            id: 6,
-            title: "Meeting F",
-            start: "2019-01-01T12:00:00+0000",
-            end: "2019-01-01T12:45:00+0000"
-          },
-          {
-            id: 8,
-            title: "Meeting H",
-            start: "2019-01-01T14:00:00+0000",
-            end: "2019-01-01T15:30:00+0000"
-          }
-        ]
-      }
-    ]);
-  });
+        {
+          end_time: "2019-01-01T11:00:00.000Z",
+          start_time: "2019-01-01T10:17:00.000Z"
+        },
+        {
+          end_time: "2019-01-01T14:00:00.000Z",
+          start_time: "2019-01-01T13:45:00.000Z"
+        },
+        {
+          end_time: "2019-01-01T23:59:59.000Z",
+          start_time: "2019-01-01T16:30:00.000Z"
+        }
+      ]);
+    });
 
-  test("getMeetingTimes accepts an array of ids with with a start and end time", () => {
-    const interval = Interval.fromISO(
-      "2019-01-01T00:00:00+0000/2019-01-01T23:59:59+0000"
-    );
-    const users = findUsers(1, 2, 3);
-    const meetingIntervals = getMeetingTimes(interval, users);
-    const meetingResponse = transformMeetingTimes(meetingIntervals);
-    expect(meetingResponse).toEqual([
-      {
-        end_time: "2019-01-01T09:00:00.000Z",
-        start_time: "2019-01-01T00:00:00.000Z"
-      },
-      {
-        end_time: "2019-01-01T11:00:00.000Z",
-        start_time: "2019-01-01T10:17:00.000Z"
-      },
-      {
-        end_time: "2019-01-01T14:00:00.000Z",
-        start_time: "2019-01-01T13:45:00.000Z"
-      },
-      {
-        end_time: "2019-01-01T23:59:59.000Z",
-        start_time: "2019-01-01T16:30:00.000Z"
-      }
-    ]);
-  });
+    test("empty request", async () => {
+      await appTest.get("/available_times").expect(400);
+    });
 
-  test("empty request", async () => {
-    await appTest.get("/available_times").expect(400);
-  });
-
-  test("valid request", async () => {
-    await appTest
-      .get("/available_times")
-      .send({
-        user_ids: [1, 2, 3],
-        start: "2019-01-01T00:00:00+0000",
-        end: "2019-01-01T23:59:59+0000"
-      })
-      .expect(200, {
-        responseBody: [
-          {
-            end_time: "2019-01-01T09:00:00.000Z",
-            start_time: "2019-01-01T00:00:00.000Z"
-          },
-          {
-            end_time: "2019-01-01T11:00:00.000Z",
-            start_time: "2019-01-01T10:17:00.000Z"
-          },
-          {
-            end_time: "2019-01-01T14:00:00.000Z",
-            start_time: "2019-01-01T13:45:00.000Z"
-          },
-          {
-            end_time: "2019-01-01T23:59:59.000Z",
-            start_time: "2019-01-01T16:30:00.000Z"
-          }
-        ]
-      });
+    test("valid request", async () => {
+      await appTest
+        .get("/available_times")
+        .send({
+          user_ids: [1, 2, 3],
+          start: "2019-01-01T00:00:00+0000",
+          end: "2019-01-01T23:59:59+0000"
+        })
+        .expect(200, {
+          responseBody: [
+            {
+              end_time: "2019-01-01T09:00:00.000Z",
+              start_time: "2019-01-01T00:00:00.000Z"
+            },
+            {
+              end_time: "2019-01-01T11:00:00.000Z",
+              start_time: "2019-01-01T10:17:00.000Z"
+            },
+            {
+              end_time: "2019-01-01T14:00:00.000Z",
+              start_time: "2019-01-01T13:45:00.000Z"
+            },
+            {
+              end_time: "2019-01-01T23:59:59.000Z",
+              start_time: "2019-01-01T16:30:00.000Z"
+            }
+          ]
+        });
+    });
   });
 });
