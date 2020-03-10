5af71f2dabb68896af0eb99486e45b8fa5228c7c
Robert Ruttenberg <rob@strivelabs.com>
2020-03-10 16:02:18 -0400

    refactor error handling

--- src/app.ts
+++ src/app.ts
@@ -27,27 +27,25 @@ app.get("/available_times", (req, res, next) => {
       start: "ISO 8601 datetime",
       end: "ISO 8601 datetime"
     };
-    return res.status(400).send({
-      error: `${util.inspect(
-        req.body
-      )} is not a valid request body. Request body should be of the form \n ${util.inspect(
-        template
-      )}\n`
-    });
+    return next(
+      createHttpError(
+        400,
+        `Request should be of the form ${JSON.stringify(template)}`
+      )
+    );
   }
   next();
 });
 
 app.get("/available_times", (req, res, next) => {
   const users = findUsers(...req.body.user_ids);
+  if (users.length < 1) {
+    next(createHttpError(404, "No users found"));
+  }
   const timeWindow = Interval.fromISO(`${req.body.start}/${req.body.end}`);
   const meetingTimes = getMeetingTimes(timeWindow, users);
   const responseBody = transformMeetingTimes(meetingTimes);
   res.send({ responseBody });
-});
-
-app.use(function(req, res, next) {
-  next(createHttpError(404));
 });
 
 // error handler
@@ -58,7 +56,7 @@ app.use(function(
   next: NextFunction
 ) {
   res.status(err.status || 500);
-  res.send({ err });
+  res.send({ error: err });
 });
 
 export default app;
